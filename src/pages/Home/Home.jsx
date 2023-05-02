be09b4eb7b4c27ad47b31cc5df45af5ad34cbfc4
morshedalam39 <morshedalam4239@gmail.com>
2023-05-02 10:18:33 +0600

    banner add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Home/Banner/Banner';
 
 const Home = () => {
     return (
-        <div>
-            <h1>this is home</h1>
+        <div className='px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 '>
+            <Banner></Banner>
         </div>
     );
 };
